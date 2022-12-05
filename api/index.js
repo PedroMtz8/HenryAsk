c7d9e9b38bb06828bf22981318d180446cec593b
NachoPasser <nachopasser@gmail.com>
2022-12-05 13:45:31 -0300

    feat(middlewares, index, package.json): cree y configure el servidor añadi un middleware para todas las rutas de posts que valida el token agregué express, morgan, cors y nodemon como dependencia

---
+++ api/index.js
@@ -0,0 +1,19 @@
+require('./connection')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const env = require('./src/config/index') //contiene todas las variables de entorno
+const { verifyToken } = require('./src/controllers/middlewares')
+
+const server = express()
+server.use(cors())
+server.use(morgan('dev'))
+server.use(express.json());
+
+// Rutas
+server.use('/auth', require('./src/routes/auth'))
+server.use(verifyToken) //Las siguientes rutas contienen este middleware en cada peticion
+server.use('/posts', require('./src/routes/posts'))
+
+
+server.listen(env.PORT, () => console.log(`Server iniciado en ${env.PORT}!`))
