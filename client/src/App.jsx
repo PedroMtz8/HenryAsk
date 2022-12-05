62fb82bf2c6b3dce16267cc7cba5305112541c10
nicoseper89 <nicosepertino@hotmail.com>
2022-12-04 21:21:00 -0300

    login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import Home from "./Components/Home/Home"
+import Login from "./Components/Login/Login"
+import {Route, Routes} from 'react-router-dom'
+import Register from "./Components/Register/Register"
+
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Register/>} />
+    </Routes>
   )
 }
 
