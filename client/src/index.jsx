3cedea9961a21039b2a6a27ad47751e6308af83c
David <david.cicconi94@gmail.com>
2022-12-06 09:58:22 -0300

    slices created

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
+);
---
+++ client/src/slices/userSlice.js
@@ -0,0 +1,10 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default userSlice.reducer;
