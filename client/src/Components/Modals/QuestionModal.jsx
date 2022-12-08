f5df7ec3bf4cc30f847ecb0f84d1fb106d8a9a7f
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2022-12-07 23:40:27 -0600

    navbar con modal

---
+++ client/src/Components/Modals/QuestionModal.jsx
@@ -0,0 +1,64 @@
+import { 
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormLabel,
+  Input,
+  FormControl
+ } from "@chakra-ui/react"
+
+ import { useRef } from "react"
+
+
+export default function QuestionModal({title}) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const initialRef = useRef(null)
+  const finalRef = useRef(null)
+
+  return (
+    <>
+      <Button backgroundColor={"#FFFF01"}/*  background={}  */onClick={onOpen}>{title}</Button>
+     {/*  <Button ml={4} ref={finalRef}>
+        I'll receive focus on close
+      </Button> */}
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Last name</FormLabel>
+              <Input placeholder='Last name' />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,90 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Img,
+  HStack,
+  VStack,
+  Text
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import QuestionModal from '../Modals/QuestionModal';
+import { Link as RouteLink } from 'react-router-dom';
+import { useAuth } from '../AuthComponents/AuthContext';
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { signout } = useAuth()
+
+  const logout = async() => {
+    await signout()
+  }
+
+  return (
+    <>
+      <Box bg={useColorModeValue('black', 'black')} px={4}>
+        <Flex h={"96px"} alignItems={'center'} justifyContent={'space-around'}>
+          <Img h={"25px"} src='https://assets.soyhenry.com/henry-landing/assets/Henry/logo-white.png' />
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7} margin={0}>
+            <QuestionModal title={"Hacer pregunta"} />
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            
+                <Flex>
+
+              <Menu>
+                <MenuButton
+                  transition="all 0.3s"
+                  _focus={{ boxShadow: 'none' }}>
+                  <Avatar
+                    size={'md'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                </MenuButton>
+                <MenuList
+                    bg={useColorModeValue('white', 'gray.900')}
+                    borderColor={useColorModeValue('gray.200', 'gray.700')}
+                  >
+                    <br />
+                    <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <Text>Username</Text>
+                  </Center>
+                    <RouteLink to={'/profile'}>
+                      <MenuItem> Profile </MenuItem>
+                    </RouteLink>
+                    <MenuDivider />
+                    <MenuItem onClick={logout} >Log out</MenuItem>
+                  </MenuList>
+              </Menu>
+              </Flex>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
