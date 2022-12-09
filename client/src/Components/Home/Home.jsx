fd5fc9c3d9d9a4e4cb41bd1b29e2b1f4fb790757
NicoSeper <nicosepertino@hotmail.com>
2022-12-09 15:44:57 -0300

    creating the home and footer components

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,19 @@
+import { Flex } from '@chakra-ui/react'
+import Footer from './Footer/Footer'
 
 const Home = () => {
 
-    return(
-        <div>
-            Este es el home
-        </div>
+    return (
+        <Flex h="150vh"
+            maxW="100vw"
+            bg="#1F1F1F"
+            flexDirection="column"
+            justifyContent="space-between">
+            <Flex bg="#000000"
+                h="22vh"  >
+            </Flex>
+            <Footer />
+        </Flex>
     )
 }
 
