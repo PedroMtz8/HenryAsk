12c3cbff66e1a6a62c1b8e26e39677a9a19341d6
NachoPasser <nachopasser@gmail.com>
2022-12-13 00:24:56 -0300

    feat(controllers/users, routes/auth): controller: elimine functions elimine funcion para aprovar usuarios añadi funcion para obtener usuarios y buscarlos por mail añádi funcion para registrar administradores route: añadi ruta para obtener usuarios (token + ser admin) añadi ruta para registrar admins (token + ser admin)

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { verifyToken, checkAdmin } = require('../controllers/middlewares');
-const { registerUser, approveUser, getUserById, checkStatusUser } = require('../controllers/users')
+const { registerUser, getUserById, checkStatusUser, getUsers, registerAdmin } = require('../controllers/users')
 
+router.get('/users', verifyToken, checkAdmin, getUsers)
 router.get('/status', verifyToken, checkStatusUser)
 router.get('/', verifyToken, getUserById)
 router.post('/register', registerUser)
-router.put('/approve', verifyToken, checkAdmin, approveUser)
+router.post('/register/admin', verifyToken, checkAdmin, registerAdmin) //tenemos un superadmin registrado en la base de datos
 
 module.exports = router
