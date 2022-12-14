e0c20c94ded51f060045b1b61c14c24258458ab4
David <david.cicconi94@gmail.com>
2022-12-14 14:01:10 -0300

    add register messagge reducer

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -1,14 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isAuth: false,
+  message: "",
 };
+
+export const register = createAsyncThunk("user/register", async () => {
+  try {
+    const { data } = await axios.post("http://localhost:3001/auth/register");
+    console.log(data.message);
+    return data.message;
+  } catch (error) {
+    return error;
+  }
+});
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.isAuth = false;
+      state.message = action.payload.message;
+    });
+  },
 });
 
 export default authSlice.reducer;
