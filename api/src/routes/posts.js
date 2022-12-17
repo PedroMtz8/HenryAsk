785549895a1288becd4d104455e0754c7be72418
NachoPasser <nachopasser@gmail.com>
2022-12-16 22:48:55 -0300

    feat(controllers/posts, routes/posts): routes: añadi ruta para obtener detalle de un post controllers: hice varios cambios: editar post: valido si el editor es el autor del post obtener post por usuario: añadi paginado y ordenes, el user viene porquery obtener posts: limite la cantidad de datos que devuelve arregle la obtencion del numero maximo de pagina obtener detalle de post: nueva funcion para obtener detalle de un post

--- api/src/routes/posts.js
+++ api/src/routes/posts.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getPostsByUserId, createPost, editPost, getPosts, sumPostScore } = require('../controllers/posts');
+const { getPostsByUserId, createPost, editPost, getPosts, sumPostScore, getPostDetail } = require('../controllers/posts');
 
 router.get('/', getPosts)
 router.get('/user', getPostsByUserId)
+router.get('/:id', getPostDetail)
 router.post('/', createPost)
 router.put('/sum', sumPostScore)
 router.put('/', editPost)
