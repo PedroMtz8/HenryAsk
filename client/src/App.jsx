9b8071a121363d1b921bd78498bf6e1259831600
David <david.cicconi94@gmail.com>
2022-12-17 21:45:04 -0300

    agregadas las url de admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Profile from "./Components/Profile/Profile";
 import CardsHome from "./Components/Card/CardHome";
 import Posts from "./Components/Posts/Posts";
 import HomeAdmin from "./Components/Admin/HomeAdmin";
+import Accounts from "./Components/Admin/Accounts";
+import Reports from "./Components/Admin/Reports";
 
 function App() {
   return (
@@ -18,8 +20,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/posts" element={<Posts />} />
+        // Rutas de admin
         <Route path="/admin" element={<HomeAdmin />} />
-
+        <Route path="/admin/accounts" element={<Accounts />} />
+        <Route path="/admin/reports" element={<Reports />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/home" element={<Home />} />
           <Route path="/card" element={<CardsHome />} />
--- client/src/Components/Admin/Accounts.jsx
+++ client/src/Components/Admin/Accounts.jsx
@@ -1,14 +1,15 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
-const HomeAdmin = () => {
+const Accounts = () => {
   return (
     <Flex>
       <Sidebar />
-      <h2>Este es el home de admin</h2>
+      <h2>Estas son las cuentas</h2>
     </Flex>
   );
 };
 
-export default HomeAdmin;
+export default Accounts;
