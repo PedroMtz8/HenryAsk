6c1b20d21f495af93fea585ace5bb4843331f04e
NachoPasser <nachopasser@gmail.com>
2022-12-20 20:30:04 -0300

    feat(routes: answer, posts): añadi ruta put para votar posts y respuestas

--- api/src/routes/answer.js
+++ api/src/routes/answer.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { getAnswersFromUser, createAnswer, editAnswer, getAnswersFromPost } = require('../controllers/answers');
+const { getAnswersFromUser, createAnswer, editAnswer, getAnswersFromPost, voteAnswer } = require('../controllers/answers');
 
 router.get('/post', getAnswersFromPost)
 router.get('/user', getAnswersFromUser)
 router.post('/', createAnswer)
+router.put('/:type', voteAnswer)
 router.put('/', editAnswer)
 
 module.exports = router
