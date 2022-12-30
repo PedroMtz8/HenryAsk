723c35dade9b9d6526395e917ee58ec762678c7a
NachoPasser <nachopasser@gmail.com>
2022-12-30 19:10:40 -0300

    feat(package, SearchBar): package: nueva libreria useWindowWidth SearchBar: usando useWindowWidth separe la barra de busqueda en dos componentes ModalFilters: la barra muestra 2 campos: orden y un boton filter que renderiza un modal que contiene todos los filtros SelectFilters: la barra muestra 4 campos: busqueda por titulo, orden, modulo y tags

--- client/src/Components/Home/Paginated/SearchBar/SearchBar.jsx
+++ client/src/Components/Home/Paginated/SearchBar/SearchBar.jsx
@@ -1,69 +1,23 @@
 import {
-    HStack,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Text,
     FormControl,
-    Select
 } from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
-import { useDispatch } from 'react-redux'
-import { changeModuleFilter, changeOrder, changeTitleFilter } from '../../../../slices/paginatedSlice'
-import FilterTags from './FilterTags/FilterTags'
-import { useState } from 'react'
+import {
+    useWindowWidth,
+} from '@react-hook/window-size'
+import SelectFilters from './SelectFilters/SelectFilters'
+import ModalFilters from './ModalFilters/ModalFilters'
 
 const SearchBar = () => {
 
-    const dispatch = useDispatch()
-
-    const [searchInput, setSearchInput] = useState("")
-
+    const widthScreen = useWindowWidth()
     return (
-        <FormControl w="70%" fontSize=".9rem" >
-            <HStack position="relative"
-                align="center"
-                bg="#F2F2F2"
-                p="1% 2%"
-                spacing="1%"
-                borderRadius="1.5rem">
-                <InputGroup w="32%" >
-                    <Input value={searchInput}
-                        onChange={e => setSearchInput(e.target.value)}
-                        onKeyDown={e => (e.key === "Enter") ? dispatch(changeTitleFilter(searchInput)) : null}
-                        placeholder='Buscar'
-                        borderRadius="10rem" />
-                    <InputRightElement width='3rem'>
-                        <SearchIcon
-                            fontSize="1.1rem"
-                            onClick={e => dispatch(changeTitleFilter(searchInput))} />
-                    </InputRightElement>
-                </InputGroup>
-                <HStack w="32%">
-                    <Text w="8rem">
-                        Ordenar por:
-                    </Text>
-                    <Select borderRadius="10rem"
-                        onChange={e => dispatch(changeOrder(e.target.value))}>
-                        <option value={"newest"}>Más recientes</option>
-                        <option value={"score"}>Puntuación</option>
-                    </Select>
-                </HStack>
-                <HStack w="32%">
-                    <Text w="8rem">
-                        Filtrar por:
-                    </Text>
-                    <Select borderRadius="10rem"
-                        placeholder='Modulos'
-                        onChange={e => dispatch(changeModuleFilter(e.target.value))}>
-                        <option value={"M1"}>M1</option>
-                        <option value={"M2"}>M2</option>
-                        <option value={"M3"}>M3</option>
-                        <option value={"M4"}>M4</option>
-                    </Select>
-                    <FilterTags/> 
-                </HStack>
-            </HStack>
+        <FormControl w={{ base: '90%', 'lg': '70%' }} fontSize=".9rem" >
+            {
+                widthScreen > 680
+                    ? <SelectFilters />
+                    : <ModalFilters />
+            }
+
         </FormControl>
     )
 }
