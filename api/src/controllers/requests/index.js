4de05a6ecbfd5b1e35a1ae34759263bf31e86a24
NachoPasser <nachopasser@gmail.com>
2023-01-02 18:25:16 -0300

    feat(controllers/requests, routes/request): routes: cambie /register por /registro requests: checkField: nueva funcion para verificar datos de entrada getRequests: agregue nuevo filtro type

--- api/src/controllers/requests/index.js
+++ api/src/controllers/requests/index.js
@@ -4,8 +4,38 @@ const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-handlebars');
 const path = require('path')
 
+function checkFields(fields) {
+    for (const field in fields) {
+        const value = fields[field]
+        switch (field) {
+            case 'rol':
+                if(!value) return;
+                const roles = ['Administrador', 'Estudiante', 'Graduado', 'TA', 'Henry Hero']
+                if(!roles.includes(value)) return 'Rol invalido'
+                break;
+            case 'approve':
+                if(!value) return 'Debe especificar el tipo de aprobado'
+                if(value !== true && value !== false) return 'approve debe ser true o false'
+                break;
+            case 'type':
+                if(value && value !== 'Rol' && value !== 'Registro') return 'Tipo de pedido invalido.'
+            case 'page':
+                if(!value) return 'Numero de pagina requerido'
+                if (value <= 0) return 'Pagina no puede ser menor o igual a 0'
+                break;
+            case 'rid':
+                if(!value) return 'Id de request requerido'
+            default:
+                break;
+        }
+    }
+    return ''
+}
+
 const createRegisterRequest = async (req, res) => {
     const { rol } = req.body
+    const message = checkFields({ rol })
+    if (message) return res.status(400).json({ message })
     try {
         const duplicated = await Request.findOne({ user: req.id })
         if (duplicated) return res.status(409).json({ message: `Usuario ${req.id} ya tiene un pedido pendiente!` })
@@ -19,6 +49,8 @@ const createRegisterRequest = async (req, res) => {
 
 const createRolRequest = async (req, res) => {
     const { rol } = req.body
+    const message = checkFields({ rol })
+    if (message) return res.status(400).json({ message })
     try {
         const duplicated = await Request.findOne({ user: req.id })
         if (duplicated) return res.status(409).json({ message: `Usuario ${req.id} ya tiene un pedido pendiente!` })
@@ -32,6 +64,8 @@ const createRolRequest = async (req, res) => {
 
 const completeRegisterRequest = async (req, res) => {
     const { rid, approve, reason } = req.body
+    const message = checkFields({ rid, approve })
+    if (message) return res.status(400).json({ message })
     try {
         const request = await Request.findById(rid)
         if (!request) return res.status(404).json({ message: 'Pedido no existe o ya fue completado' })
@@ -67,6 +101,7 @@ const completeRegisterRequest = async (req, res) => {
         };
 
         if (!approve) {
+            if(!reason) return res.status(400).json({message: 'Debe incluir una razon'})
             mailOptions.template = 'registerR'
             mailOptions.context = { reason }
             response = { message: 'Peticion rechazada!' }
@@ -94,6 +129,8 @@ const completeRegisterRequest = async (req, res) => {
 
 const completeRolRequest = async (req, res) => {
     const { rid, approve, reason } = req.body
+    const message = checkFields({ rid, approve })
+    if (message) return res.status(400).json({ message })
     try {
         const request = await Request.findById(rid)
         if (!request) return res.status(404).json({ message: 'Pedido no existe o ya fue completado' })
@@ -129,6 +166,7 @@ const completeRolRequest = async (req, res) => {
         };
 
         if (!approve) {
+            if(!reason) return res.status(400).json({message: 'Debe incluir una razon'})
             mailOptions.template = 'rolR'
             mailOptions.context = { reason }
             response = { message: 'Peticion rechazada!' }
@@ -155,22 +193,42 @@ const completeRolRequest = async (req, res) => {
 }
 
 const getRequests = async (req, res) => {
-    const { page } = req.query
-    if (!page) return res.status(400).json({ message: 'Numero de pagina requerido.' })
+    const { page, type} = req.query
+    const message = checkFields({ page })
+    if (message) return res.status(400).json({ message })
 
-    try {
-        const numberOfDocs = await Request.countDocuments()
-        const maxPages = Math.ceil(numberOfDocs / 10)
-        const requests = await Request.find()
-            .sort({ createdAt: 1 })
-            .skip(page * 10 - 10)
-            .limit(10)
-            .populate('user', { mail: 1, userSlack: 1, rol: 1 })
-        //te devuelve las requests ordenadas de mas viejas a mas nuevas
-        res.json({ message: 'Pedidos encontrados!', requests, maxPages })
-    } catch (error) {
-        res.json({ message: error.message })
+    function buildQuery() {
+        let query = ''
+
+        if (type) {
+            query = query
+                ? query.find({ type })
+                : Request.find({ type })
+        }
+
+        if(!query) query = Request.find()
+        return query
     }
+    //busqueda para obtener usuarios paginados
+    const searchPaginatedRequests = buildQuery()
+        .skip(page * 10 - 10)
+        .limit(10)
+        .sort({ createdAt: 1 })
+        .populate('user', { mail: 1, userSlack: 1, rol: 1 })
+
+    //busqueda para obtener numero maximo de pagina
+    const searchAllRequests = buildQuery().select({ _id: 1 })
+
+    //Ejecuto dos busquedas al mismo tiempo
+    Promise.all([
+        searchPaginatedRequests,
+        searchAllRequests
+    ]).then(([paginatedDocs, allDocs]) => {
+        const maxPages = Math.ceil(allDocs.length / 10)
+        res.json({ message: 'Pedidos encontrados!', requests: paginatedDocs, maxPages })
+    }).catch(error => {
+        res.json({ message: error.message })
+    })
 }
 
 module.exports = {
--- api/src/routes/request.js
+++ api/src/routes/request.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { checkAdmin } = require('../controllers/middlewares/index')
 const { createRegisterRequest, createRolRequest, completeRegisterRequest, completeRolRequest, getRequests } = require('../controllers/requests')
 
-router.post('/register', createRegisterRequest)
-router.put('/register', checkAdmin, completeRegisterRequest)
+router.post('/registro', createRegisterRequest)
+router.put('/registro', checkAdmin, completeRegisterRequest)
 router.post('/rol', createRolRequest)
 router.put('/rol', checkAdmin, completeRolRequest)
 router.get('/', checkAdmin, getRequests)
